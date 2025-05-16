79a1423293d7f8acfa7d642ec0aa6901ec118b81
Ivan <kowd.pauuh@gmail.com>
2025-05-16 17:27:14 +0000

    Declared distanceFns unordered map

--- include/distances.h
+++ include/distances.h
@@ -1,10 +1,14 @@
 #ifndef DISTANCES_H
 #define DISTANCES_H
 
+#include <unordered_map>
+#include <string>
+#include <functional>
 #include <Eigen/Dense>
 
 double euclidean(const Eigen::VectorXd& a, const Eigen::VectorXd& b);
 double manhattan(const Eigen::VectorXd& a, const Eigen::VectorXd& b);
 double cosine(const Eigen::VectorXd& a, const Eigen::VectorXd& b);
+extern const std::unordered_map<std::string, std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd&)>> distanceFns;
 
 #endif // DISTANCES_H
