a0b79b0ea42c6e1d73e83945b126a9ceba317a9c
Ivan <kowd.pauuh@gmail.com>
2025-05-16 16:10:17 +0000

    Added input printing

--- dbsod_cpp/dbsod.cpp
+++ dbsod_cpp/dbsod.cpp
@@ -7,4 +7,5 @@ void dbsod(double* data, int rows, int cols) {
     Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> X(data, rows, cols);
 
     std::cout << "Hello from DBSOD!\n";
+    std::cout << X << std::endl;
 }
