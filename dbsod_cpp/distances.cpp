812f59dc38385c5aafa8bb208de48500c439115f
Ivan <kowd.pauuh@gmail.com>
2025-05-16 16:40:44 +0000

    Implemented functions for cosine and manhattan distances computation

--- dbsod_cpp/distances.cpp
+++ dbsod_cpp/distances.cpp
@@ -4,3 +4,11 @@
 double euclidean(const Eigen::VectorXd& a, const Eigen::VectorXd& b) {
     return (a - b).norm();
 }
+
+double manhattan(const Eigen::VectorXd& a, const Eigen::VectorXd& b) {
+    return (a - b).cwiseAbs().sum();
+}
+
+double cosine(const Eigen::VectorXd& a, const Eigen::VectorXd& b) {
+    return 1.0 - (a.dot(b) / (a.norm() * b.norm()));
+}
