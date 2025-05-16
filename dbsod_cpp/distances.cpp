d0276ed538c6a7ed2c5c97927c1afce37860dcf6
Ivan <kowd.pauuh@gmail.com>
2025-05-16 17:31:42 +0000

    Added comments

--- dbsod_cpp/distances.cpp
+++ dbsod_cpp/distances.cpp
@@ -4,14 +4,17 @@
 #include <unordered_map>
 #include <Eigen/Dense>
 
+// compute euclidean (L2) distance between two vectors
 double euclidean(const Eigen::VectorXd& a, const Eigen::VectorXd& b) {
     return (a - b).norm();
 }
 
+// compute manhattan (L1) distance between two vectors
 double manhattan(const Eigen::VectorXd& a, const Eigen::VectorXd& b) {
     return (a - b).cwiseAbs().sum();
 }
 
+// compute cosine distance between two vectors
 double cosine(const Eigen::VectorXd& a, const Eigen::VectorXd& b) {
     return 1.0 - (a.dot(b) / (a.norm() * b.norm()));
 }
