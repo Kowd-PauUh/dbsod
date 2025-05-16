0b0544c00cdc0377d38d9f2e60ef9658dabe54a6
Ivan <kowd.pauuh@gmail.com>
2025-05-16 17:28:40 +0000

    Added distance function selection based on metric value

--- dbsod_cpp/dbsod.cpp
+++ dbsod_cpp/dbsod.cpp
@@ -1,11 +1,24 @@
 #include "dbsod.h"
 #include <iostream>
 #include <string>
+#include <functional>
 #include <Eigen/Dense>
 #include "distances.h"
 
-void dbsod(double* data, int rows, int cols, std::string metric) {
+void dbsod(double* data, int rows, int cols, const char* metric) {
     Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> X(data, rows, cols);
+    std::string metricStr(metric);
+
+    // get distance function based on metric argument
+    std::function<double(const Eigen::VectorXd& a, const Eigen::VectorXd& b)> distanceFn;
+    auto it = distanceFns.find(metricStr);
+    if (it != distanceFns.end()) {
+        distanceFn = it->second;
+    } else {
+        throw std::runtime_error(
+            "\"metric\" argument must be one of [\"euclidean\", \"manhattan\", \"cosine\"]"
+        );
+    }
 
     std::cout << "Hello from DBSOD!\n";
     std::cout << X << std::endl;
