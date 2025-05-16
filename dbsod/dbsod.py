0dc1d7536a08b26c9a61086170a379b05358b4b1
Ivan <kowd.pauuh@gmail.com>
2025-05-16 17:26:53 +0000

    Added metric argument

--- dbsod/dbsod.py
+++ dbsod/dbsod.py
@@ -1,5 +1,6 @@
 import ctypes
 from importlib import resources
+from typing import Literal
 
 import numpy as np
 
@@ -9,9 +10,15 @@ dbsod_cpp = ctypes.CDLL(dbsod_cpp_path)
 dbsod_cpp.dbsod.restype = None
 
 
-def dbsod(X: np.ndarray):
+def dbsod(
+    X: np.ndarray,
+    metric: Literal['euclidean', 'manhattan', 'cosine'] = 'cosine',
+):
     X = X.astype(np.float64)
-
-    X_ptr = X.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
     rows, cols = X.shape
-    return dbsod_cpp.dbsod(X_ptr, rows, cols)
+
+    # pointers for unsupported data types
+    X_ptr = X.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    metric_ptr = ctypes.c_char_p(metric.encode('utf-8'))
+
+    return dbsod_cpp.dbsod(X_ptr, rows, cols, metric_ptr)
