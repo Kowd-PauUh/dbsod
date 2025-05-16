ec89e87c5d450dca42ba53daa76e4530dd9f9b45
Ivan <kowd.pauuh@gmail.com>
2025-05-16 16:43:53 +0000

    Added input argument metric

--- dbsod_cpp/dbsod.cpp
+++ dbsod_cpp/dbsod.cpp
@@ -1,9 +1,10 @@
 #include "dbsod.h"
 #include <iostream>
+#include <string>
 #include <Eigen/Dense>
 #include "distances.h"
 
-void dbsod(double* data, int rows, int cols) {
+void dbsod(double* data, int rows, int cols, std::string metric) {
     Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> X(data, rows, cols);
 
     std::cout << "Hello from DBSOD!\n";
