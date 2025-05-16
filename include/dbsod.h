faba37a0cb424ebdc7ccc650a601e52051937173
Ivan <kowd.pauuh@gmail.com>
2025-05-16 16:43:39 +0000

    Updated function input args

--- include/dbsod.h
+++ include/dbsod.h
@@ -1,8 +1,10 @@
 #ifndef DBSOD_H
 #define DBSOD_H
 
+#include <string>
+
 extern "C" {
-    void dbsod(double* data, int rows, int cols);
+    void dbsod(double* data, int rows, int cols, std::string metric);
 }
 
 #endif // DBSOD_H
