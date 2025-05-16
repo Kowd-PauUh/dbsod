c95049d7701870833e82a96f9941d06275d4c4dd
Ivan <kowd.pauuh@gmail.com>
2025-05-16 15:42:24 +0000

    Implemented array passing to dbsod_cpp

--- dbsod/dbsod.py
+++ dbsod/dbsod.py
@@ -1,9 +1,17 @@
 import ctypes
 from importlib import resources
 
+import numpy as np
+
 
 dbsod_cpp_path = resources.files('dbsod.build').joinpath('dbsod.so')
 dbsod_cpp = ctypes.CDLL(dbsod_cpp_path)
 dbsod_cpp.dbsod.restype = None
 
-__all__ = ['dbsod_cpp']
+
+def dbsod(X: np.ndarray):
+    X = X.astype(np.float64)
+
+    X_ptr = X.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    rows, cols = X.shape
+    return dbsod_cpp.dbsod(X_ptr, rows, cols)
