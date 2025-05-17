912d817d82ed08003fe973f4e612c1e03b703670
Ivan <kowd.pauuh@gmail.com>
2025-05-17 08:22:20 +0000

    Added epsSpace related args

--- dbsod_cpp/dbsod.cpp
+++ dbsod_cpp/dbsod.cpp
@@ -1,28 +1,46 @@
 #include "dbsod.h"
+
 #include <iostream>
 #include <string>
 #include <functional>
-#include <Eigen/Dense>
-#include "distances.h"
 
-void dbsod(double* data, int rows, int cols, const char* metric) {
+#include <Eigen/Dense>
+
+#include "distances.h"
+#include "neighborhood.h"
+
+void dbsod(
+    double* dataPtr,
+    int rows,
+    int cols,
+    const char* metricPtr,
+    float* epsSpacePtr,
+    int numEpsValues
+) {
     // validate input
-    if (!data) {
+    if (!dataPtr) {
         throw std::invalid_argument("Input data pointer is null.");
+    }
+    if (!metricPtr) {
+        throw std::invalid_argument("Input metric pointer is null.");
+    }
+    if (!epsSpacePtr) {
+        throw std::invalid_argument("Input epsSpace pointer is null.");
     }
 
     // form appropriate data structures from given pointers 
-    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> X(data, rows, cols);
-    std::string metricStr(metric);
+    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> X(dataPtr, rows, cols);
+    std::string metric(metricPtr);
+    std::vector<float> epsSpace(epsSpacePtr, epsSpacePtr + numEpsValues);
 
-    // get distance function based on metric argument
+    // get distance function based on metricPtr argument
     std::function<double(const Eigen::VectorXd& a, const Eigen::VectorXd& b)> distanceFn;
-    auto it = distanceFns.find(metricStr);
+    auto it = distanceFns.find(metric);
     if (it != distanceFns.end()) {
         distanceFn = it->second;
     } else {
         throw std::runtime_error(
-            "\"metric\" argument must be one of [\"euclidean\", \"manhattan\", \"cosine\"]."
+            "\"metricPtr\" argument must point to one of [\"euclidean\", \"manhattan\", \"cosine\"]."
         );
     }
 
