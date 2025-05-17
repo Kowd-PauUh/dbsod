401e859ab9b9bcddcfc91eac6cc08eeae2481eae
Ivan <kowd.pauuh@gmail.com>
2025-05-17 12:19:14 +0000

    Added pointer return on results array. Implemented free_array.

--- dbsod_cpp/dbsod.cpp
+++ dbsod_cpp/dbsod.cpp
@@ -10,7 +10,7 @@
 #include "distances.h"
 #include "neighborhood.h"
 
-void dbsod(
+float* dbsod(
     double* dataPtr,
     int rows,
     int cols,
@@ -51,9 +51,22 @@ void dbsod(
     float maxEps = *std::max_element(epsSpace.begin(), epsSpace.end());
     std::vector<std::vector<std::pair<int, float>>> neighbors = brute(X, distanceFn, maxEps);
 
+    // compute outlierness scores
+    std::vector<float> scores(rows, 0.0f);
+
+    // allocate memory to return
+    float* result = new float[rows];
+    std::copy(scores.begin(), scores.end(), result);
+
     // debug
     std::cout << "Hello from DBSOD!\n";
     std::cout << X << std::endl;
     std::cout << "maxEps=" << maxEps << std::endl;
     std::cout << "minPts=" << minPts << std::endl;
+
+    return result;
+}
+
+void free_array(float* arrayPtr) {
+    delete[] arrayPtr;
 }
