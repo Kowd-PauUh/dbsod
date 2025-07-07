b0bf6d88de0ad8846bef37d49bcc63dfecf6b857
Ivan <kowd.pauuh@gmail.com>
2025-07-07 07:38:00 +0000

    Added outliers function declaration

---
+++ include/outliers.h
@@ -0,0 +1,13 @@
+#ifndef OUTLIERS_H
+#define OUTLIERS_H
+
+#include "outliers.h"
+#include <vector>
+
+std::vector<int> outliers(
+    std::vector<std::vector<std::pair<int, float>>>& neighbors,
+    int minPts,
+    float eps
+);
+
+#endif // OUTLIERS_H
