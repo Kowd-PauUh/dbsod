864ed5772e8cab48e01843a0fd444f4d1b3d25fd
Ivan <kowd.pauuh@gmail.com>
2026-04-09 11:03:50 +0200

    Added setup.py to streamline .so build

---
+++ setup.py
@@ -0,0 +1,24 @@
+import glob
+
+from setuptools import setup, Extension
+import pybind11
+
+
+ext_modules = [
+    Extension(
+        "dbsod.dbsod_cpp",
+        sources=glob.glob("dbsod_cpp/*.cpp"),
+        include_dirs=[
+            "include",
+            "/usr/include/eigen3",
+            pybind11.get_include(),
+        ],
+        language="c++",
+        extra_compile_args=["-O3", "-std=c++23", "-Wall", "-fPIC"],
+    )
+]
+
+setup(
+    ext_modules=ext_modules,
+    zip_safe=False,
+)
