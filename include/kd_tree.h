516df024f30711d559a09cc58aa5d95f9674d812
Ivan <kowd.pauuh@gmail.com>
2026-04-13 12:51:17 +0200

    Declared KDTree

---
+++ include/kd_tree.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2026 Ivan Danylenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <span>
+
+namespace kd_tree {
+
+struct Neighbor {
+    size_t index;
+    double dist;
+};
+
+class KDTree {
+  private:
+    struct TreeNode {
+        size_t index;
+        size_t axis;
+        std::unique_ptr<TreeNode> left;
+        std::unique_ptr<TreeNode> right;
+    };
+
+    std::span<const double> data;  // row-major contiguous layout
+    size_t rows, cols;
+    std::unique_ptr<TreeNode> root;
+
+    double get_value(size_t row, size_t col) const;
+
+  public:
+    KDTree(const std::span<const double> &data_, size_t rows_, size_t cols_);
+    std::vector<Neighbor> query_radius(const std::vector<double> &query, double r) const;
+};
+    
+}  // namespace kd_tree
