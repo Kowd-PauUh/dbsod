3ace7baaa6d775131b90950ec81799147dbdcff7
Ivan <kowd.pauuh@gmail.com>
2026-04-13 15:02:23 +0200

    Updated "outliers" declaration to accept neighborhood graph from KDTree::radius_neighborhood_graph

--- include/outliers.h
+++ include/outliers.h
@@ -18,13 +18,16 @@
 #define OUTLIERS_H
 
 #include "outliers.h"
+
 #include <vector>
 #include <Eigen/Dense>
 
+#include "kd_tree.h"
+
 Eigen::VectorXi outliers(
-    std::vector<std::vector<std::pair<int, float>>>& neighbors,
+    std::vector<std::vector<kd_tree::Neighbor>>& neighbors,
     int minPts,
-    float eps
+    double eps
 );
 
 #endif // OUTLIERS_H
