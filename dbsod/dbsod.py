d5be4b06965f14e96b350df61f3083522daf15c3
Ivan <kowd.pauuh@gmail.com>
2026-04-13 15:29:51 +0200

    Updated license notice

--- dbsod/dbsod.py
+++ dbsod/dbsod.py
@@ -1,5 +1,5 @@
 """
-Copyright 2025 Ivan Danylenko
+Copyright 2026 Ivan Danylenko
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
