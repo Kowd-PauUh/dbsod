794450a2d91fb2d6b759b189c0e2c92b656fb5ee
Ivan <kowd.pauuh@gmail.com>
2026-04-15 17:05:25 +0200

    Added DBSOD functional tests

--- tests/test_dbsod_functional.py
+++ tests/test_dbsod_functional.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from dbsod import dbsod
+from dbsod import DBSOD, dbsod
 
 
 DATA = np.array(
@@ -18,11 +18,24 @@ MIN_PTS = 2
 EXPECTED_RESULT = np.array([0.0, 0.5, 0.0, 0.0, 1.0])
 
 
-def test_dbsod_functional():
-    result = dbsod(
-        X=DATA,
-        eps_space=EPS_SPACE,
-        min_pts=MIN_PTS,
-    )
+def test_DBSOD_fit():
+    model = DBSOD(eps_space=EPS_SPACE, min_pts=MIN_PTS)
+    result = model.fit(X=DATA).outlierness_score
+
+    assert isinstance(result, np.ndarray), "Expected result to be a numpy ndaray"
+    np.testing.assert_allclose(result, EXPECTED_RESULT, atol=1e-6)
+
+
+def test_DBSOD_fit_predict():
+    model = DBSOD(eps_space=EPS_SPACE, min_pts=MIN_PTS)
+    result = model.fit_predict(X=DATA)
+
+    assert isinstance(result, np.ndarray), "Expected result to be a numpy ndaray"
+    np.testing.assert_allclose(result, EXPECTED_RESULT, atol=1e-6)
+
+
+def test_dbsod():
+    result = dbsod(X=DATA, eps_space=EPS_SPACE, min_pts=MIN_PTS)
+    
     assert isinstance(result, np.ndarray), "Expected result to be a numpy ndaray"
     np.testing.assert_allclose(result, EXPECTED_RESULT, atol=1e-6)
